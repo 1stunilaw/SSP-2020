2c5a74a266b7f2325c27660d1d70b3037a3643b8
VDHCUR <55709587+VDHCUR@users.noreply.github.com>
2020-11-24 11:17:28 +0700

    Добавил CORS

--- src/main/java/ssp/marketplace/app/config/SecurityConfiguration.java
+++ src/main/java/ssp/marketplace/app/config/SecurityConfiguration.java
@@ -6,11 +6,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.servlet.config.annotation.*;
 import ssp.marketplace.app.security.*;
 import ssp.marketplace.app.security.jwt.*;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     private final JwtTokenProvider jwtTokenProvider;
 
