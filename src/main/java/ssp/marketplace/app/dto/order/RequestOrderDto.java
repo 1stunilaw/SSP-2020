8d5758afce6f8c6a1f2cc8ebb8bfa75f4a71094a
h4rar <anton.h4rar@gmail.com>
2020-12-26 19:45:36 +0700

    переделал валидацию даты и имени при редактировании и изменении заказа

--- src/main/java/ssp/marketplace/app/dto/order/RequestOrderDto.java
+++ src/main/java/ssp/marketplace/app/dto/order/RequestOrderDto.java
@@ -6,7 +6,7 @@ import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 import ssp.marketplace.app.entity.statuses.StatusForOrder;
-import ssp.marketplace.app.service.OrderService;
+import ssp.marketplace.app.service.*;
 import ssp.marketplace.app.validation.unique.Unique;
 
 import javax.validation.constraints.*;
@@ -25,12 +25,14 @@ public class RequestOrderDto {
     @NotNull(message = "{name.errors.empty}")
     @Length(min = 1, max = 250, message = "{name.errors.length}")
     @Pattern(regexp = "(^$)|(^[а-яА-ЯёЁa-zA-Z0-9-a-zA-ZА-я-()<>@#№$;%*_=^/{}\\[\\].,!?':\\s+&\" ]+$)", message = "{name.errors.regex}")
+    @Unique(message = "{order.errors.unique}", service = OrderService.class, fieldName = "name")
     private String name;
     //    @DateTimeFormat(pattern = "YYYY-MM-dd hh:mm")
     //    private LocalDateTime dateStart;
 
     @NotNull(message = "{dateStop.errors.empty}")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Unique(message = "{dateStop.errors.before}", service = OrderService.class, fieldName = "dateStop")
     private LocalDate dateStop;
 
     @NotBlank(message = "{description.errors.empty}")
